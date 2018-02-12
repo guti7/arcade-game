95c546210f4af6eff672a487de9a5b6b3763d56c
guti7 <7505855+guti7@users.noreply.github.com>
2018-02-11 18:10:09 -0800

    Complete app skeleton for main objects

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = 0;
 };
 
 // Update the enemy's position, required method for game
@@ -24,11 +26,31 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+  this.x = 0;
+  this.y = 0;
+};
+
+Player.prototype.update = function() {
+  console.log("updating location: how?");
+};
+
+// TODO: Refactor: Exactly as Enemy.prototype.update
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
+  console.log("handling keyboard input");
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const allEnemies = [new Enemy(), new Enemy()];
+const player = new Player();
 
 
 
